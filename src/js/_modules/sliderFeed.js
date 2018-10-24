56adb1fe9a58d2803a7d1f51486576dc26d8700f
Juan Martinez <juan@pwnkdigital.com>
2018-10-24 12:57:37 +0100

    project first push

---
+++ src/js/_modules/sliderFeed.js
@@ -0,0 +1,92 @@
+'use strict';
+var SliderFeed = function($target) {
+    var self = this;
+    self.$el = $target;
+    self.$sliderHolder = self.$el.querySelector('.ed--module-slider-holder');
+    self.$itemCount = parseInt(self.$sliderHolder.dataset.items);
+    self.$feedUrl = self.$sliderHolder.dataset.feed;
+    self.$ctaText = self.$sliderHolder.dataset.itemcta;
+    self.$includeDesc = self.$sliderHolder.dataset.includedesc;
+    if (self.$includeDesc === 'no') {
+        self.$includeDesc = false
+    } else {
+        self.$includeDesc = true
+    }
+    self.$swiperContainer = self.$el.querySelector('.swiper-container');
+    self.$slidesHolder = self.$el.querySelector('.swiper-wrapper');
+    self.$products = [];
+
+    // get feed
+    getFeed(self.$feedUrl).then(function(response) {
+        //console.log(response);
+        // grab the specified amount of items from the response, then init the swiper
+        self.$products = response.Products.List.slice(0,self.$itemCount);
+        self.initSwiper(self.$products);
+
+    }, function(error) {
+        console.error(error);
+    });
+}
+
+SliderFeed.prototype.initSwiper = function($items){
+    var self = this;
+
+    // create the slides
+    $items.forEach(function(item){
+        var slide = document.createElement('div');
+        slide.className = 'swiper-slide';
+
+        var slideImage = document.createElement('div');
+        slideImage.className = 'slideImage';
+        if (item.ImageMain.endsWith('_255.jpg')) {
+            var hiRes = item.ImageMain.replace('_255.jpg','_480.jpg');
+        } else {
+            var hiRes = item.ImageMain;
+        }
+        slideImage.style.backgroundImage = 'url('+hiRes+')';
+        slide.append(slideImage)
+
+        if (self.$includeDesc) {
+            var itemBrand = document.createElement('h3');
+            itemBrand.className = 'itemBrand';
+        } else {
+            var itemBrand = document.createElement('h2');
+            itemBrand.className = 'itemBrand styled';
+        }
+        itemBrand.innerHTML = item.DesignerName;
+        slide.append(itemBrand)
+
+        if (self.$includeDesc) {
+            var itemDesc = document.createElement('p');
+            itemDesc.className = 'itemDesc';
+            itemDesc.innerHTML = item.Description;
+            slide.append(itemDesc);
+        }
+
+        var itemLink = document.createElement('a');
+        itemLink.className = 'itemLink bold';
+        itemLink.innerHTML = self.$ctaText;
+        itemLink.href = item.ProductUrl;
+        slide.append(itemLink);
+
+        self.$slidesHolder.append(slide)
+    })
+
+    // init swiper
+    var swiper = new Swiper(self.$swiperContainer, {
+        slidesPerView: 'auto',
+        loop: true,
+        loopedSlides:4,
+        centeredSlides: true,
+        roundLengths: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+}
+
+module.exports = SliderFeed;
---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+"use strict";
+// vanilla ajax call
+window.getFeed = function(url) {
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function() {
+            if (xhr.status == 200) {
+                resolve(JSON.parse(xhr.response));
+            } else {
+                reject(Error(xhr.statusText));
+            }
+        };
+        xhr.onerror = function() {
+            reject(Error("Network Error"));
+        };
+        xhr.send();
+    });
+}
+// deps
+window.Swiper = require('swiper');
+// modules
+window.SliderFeed = require('./_modules/sliderFeed');
+window.CategorySingleFeed = require('./_modules/categorySingleFeed');
+// init
+(function(){
+    // init sliderFeeds
+    var sliderFeeds = document.querySelectorAll('.ed--module-slider-feed');
+    if (sliderFeeds.length) {
+        for (var i = 0; i < sliderFeeds.length; i++) {
+            new SliderFeed(sliderFeeds[i]);
+        }
+    }
+    // init category single product pull from feed
+    var categoryFeeds = document.querySelectorAll('.ed--module-category-single');
+    if (categoryFeeds.length) {
+        for (var i = 0; i < categoryFeeds.length; i++) {
+            new CategorySingleFeed(categoryFeeds[i]);
+        }
+    }
+})();
